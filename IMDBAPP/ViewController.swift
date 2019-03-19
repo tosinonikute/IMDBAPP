aa3baad70de72fc408042799a1ae3968e7f31a73
Tosinmath <tosinmath007@gmail.com>
2019-03-19 20:23:04 +0100

    Initial Commit

---
+++ IMDBAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IMDBAPP
+//
+//  Created by MACBOOK PRO on 19/03/2019.
+//  Copyright © 2019 MACBOOK PRO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
