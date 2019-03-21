2f9cab78c2b3c4947459f6edc15b10850b2d940d
Tosinmath <tosinmath007@gmail.com>
2019-03-21 12:27:46 +0100

    add Structs, useful constants

---
+++ IMDBAPP/Helpers/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  IMDBAPP
+//
+//  Created by MACBOOK PRO on 20/03/2019.
+//  Copyright © 2019 MACBOOK PRO. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    static let BASE_URL = "https://api.themoviedb.org/3/"
+    static let API_KEY = "0d677b16a44d2b5a79edf325bc1ee9b7"
+    static let IMAGE_URL = "https://image.tmdb.org/t/p/"
+    static let SMALL_IMG_PATH = "w200"
+    static let BIG_IMG_PATH = "w500"
+}
